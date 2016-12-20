98a94894ae58de42a8ca51cfe14904fc453ecedc
Matis Kleidman <matis@socialcodeinc.com>
2016-12-20 11:53:41 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='google_login',
+      version='0.1',
+      description='Django googe login App',
+      author='Matis Kleidman',
+      author_email='matis.kleidman@gmail.com',
+      packages=find_packages(),
+      include_package_data=True
+      )
